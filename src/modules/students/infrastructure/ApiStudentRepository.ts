f5a63d9372df2bed0bf526409e3c50b06651c408
Christian Mesina <chrmesina@icloud.com>
2024-06-05 11:58:25 -0400

    chore: Update dependencies and configuration files

--- src/modules/students/infrastructure/ApiStudentRepository.ts
+++ src/modules/students/infrastructure/ApiStudentRepository.ts
@@ -2,7 +2,27 @@ import axios from "axios";
 import { StudentRepository } from "../domain/StudentRepository";
 import { StudentModel } from "../domain/StudentSchema";
 
+
+
 export function createApiStudentRepository(): StudentRepository {
+
+   
+    
+    //   const getStudents = useQuery('students', async () => {
+    //     const { data } = await axios.get('/api/students');
+    //     return data;
+    //   });
+    
+    //   const createStudent = useMutation(async (student: StudentModel) => {
+    //     const { data } = await axios.post('/api/students', student);
+    //     return data;
+    //   });
+    
+    //   const deleteStudent = useMutation(async (id: string) => {
+    //     const { data } = await axios.delete(`/api/students/${id}`);
+    //     return data;
+    //   });
+    
 	return {
 		save,
 		findAll,
@@ -13,24 +33,24 @@ export function createApiStudentRepository(): StudentRepository {
 }
 
 async function save(student: StudentModel) {
-   await axios.post("http://localhost:3000/students", student);
+   await axios.post("/api/students", student);
 }
 
 async function findAll() {
-    const response = await axios.get("http://localhost:3000/students");
+    const response = await axios.get("/api/students");
     return response.data;
 }
 
 async function remove(id: StudentModel["id"]) {
-    await axios.delete(`http://localhost:3000/students/${id}`);
+    await axios.delete(`/api/students/${id}`);
 }
 
 async function update(id: StudentModel["id"], student: StudentModel) {
-    await axios.put(`http://localhost:3000/students/${id}`, student);
+    await axios.put(`/api/students/${id}`, student);
 }
 
 async function findById(id: StudentModel["id"])  {
-    const response = await axios.get(`http://localhost:3000/students/${id}`);
+    const response = await axios.get(`/api/${id}`);
     return response.data;
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        target: 'http://localhost:3000/students',
+        changeOrigin: false ,
+        // rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
